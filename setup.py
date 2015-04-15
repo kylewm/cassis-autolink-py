6e4f091e80a1123593fe6f8a499ff8dd073982b5
Kyle Mahan <kyle.mahan@gmail.com>
2015-04-15 09:51:40 -0700

    add travis-ci configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cassis-autolink',
+      version='0.1',
+      description='Python port of (a subset of) CASSIS',
+      author='Kyle Mahan',
+      author_email='kyle@kylewm.com',
+      url='https://github.com/kylewm/cassis-autolink-py/',
+      py_modules = ['cassis'],
+      test_suite = 'cassistest'
+)
